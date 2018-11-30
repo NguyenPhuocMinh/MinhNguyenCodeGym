c0a4d6169b028eb802f57c766967e36b8d9b6c1e
NguyenPhuocMinh <Xiuquay55@gmail.com>
2018-11-30 14:59:29 +0700

    Bài tập Sử dụng toán tử

---
+++ 02.BienVaKieuDuLieu/Thực hành sử dụng toán tử/index.js
@@ -0,0 +1,8 @@
+var width;
+var height;
+var width = parseInt(prompt("Enter the width"));
+var height = parseInt(prompt("Enter the height"));
+var area = width * height;
+function showCityName() {
+    document.write("The area is:" + area)
+}
