5be56e3b976513651fc7a8672a84449a4ac92734
NguyenPhuocMinh <Xiuquay55@gmail.com>
2018-11-30 14:27:13 +0700

    Bai Tap Minh Codegym - Bootcamp Preparation - First Commit

---
+++ 02.BienVaKieuDuLieu/Thực hành sử dụng toán tử/Dự án mới/js.js
@@ -0,0 +1,13 @@
+<body>
+    <h1>A Web Page</h1>
+    <p id="demo">A Paragraph</p>
+    <button type="button" onclick="myFunction()">Try it</button>
+    <script>
+        function myFunction() {
+            alert("Xin chào!")
+        }
+        var width;
+       var height;
+    </script>
+</body>
+
