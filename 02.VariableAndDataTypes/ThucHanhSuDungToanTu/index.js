073030bd20c83119780f21fe7150599cf4adef02
NguyenPhuocMinh <Xiuquay55@gmail.com>
2018-11-30 15:56:47 +0700

    init

--- 02.VariableAndDataTypes/ThucHanhSuDungToanTu/index.js
+++ 02.VariableAndDataTypes/ThucHanhSuDungToanTu/index.js
@@ -3,6 +3,6 @@ var height;
 var width = parseInt(prompt("Enter the width"));
 var height = parseInt(prompt("Enter the height"));
 var area = width * height;
-function showCityName() {
+function showDienTichHinhChuNhat() {
     document.write("The area is:" + area)
 }
