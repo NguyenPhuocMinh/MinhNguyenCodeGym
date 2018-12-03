edeff21e9387d13d950e0e1528670d8281e9393b
NguyenPhuocMinh <Xiuquay55@gmail.com>
2018-12-03 20:32:59 +0700

    BaiTapKiemTraNamNhuan

--- 02.VariableAndDataTypes/BaiTapUngDungMayTinh/index.js
+++ 02.VariableAndDataTypes/BaiTapUngDungMayTinh/index.js
@@ -1,24 +1,24 @@
-function addition(){
+function addition() {
     var numA = document.getElementById("numA").value;
     var numB = document.getElementById("numB").value;
     var add = parseFloat(numA) + parseFloat(numB);
-    document.getElementById("showResult").innerHTML = "Result Addition is " +  add;
+    document.getElementById("showResult").innerHTML = "Result Addition is " + add;
 }
-function subtraction(){
+function subtraction() {
     var numA = document.getElementById("numA").value;
     var numB = document.getElementById("numB").value;
     var sub = parseFloat(numA) - parseFloat(numB);
-    document.getElementById("showResult").innerHTML = "Result Subtraction is " +  sub;
+    document.getElementById("showResult").innerHTML = "Result Subtraction is " + sub;
 }
-function multiplication(){
+function multiplication() {
     var numA = document.getElementById("numA").value;
     var numB = document.getElementById("numB").value;
     var mul = parseFloat(numA) * parseFloat(numB);
-    document.getElementById("showResult").innerHTML = "Result Multiplicaition is " +  mul;
+    document.getElementById("showResult").innerHTML = "Result Multiplicaition is " + mul;
 }
-function division(){
+function division() {
     var numA = document.getElementById("numA").value;
     var numB = document.getElementById("numB").value;
     var div = parseFloat(numA) / parseFloat(numB);
-    document.getElementById("showResult").innerHTML = "Result Division is " +  div;
+    document.getElementById("showResult").innerHTML = "Result Division is " + div;
 }
