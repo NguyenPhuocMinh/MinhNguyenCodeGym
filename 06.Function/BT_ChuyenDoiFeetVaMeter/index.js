040475a17320ad07ea239153903b9f9dab1d11ba
NguyenPhuocMinh <Xiuquay55@gmail.com>
2018-12-12 14:43:10 +0700

    BT_Chuyển Đổi Giữa Feet Và Met

---
+++ 06.Function/BT_ChuyenDoiFeetVaMeter/index.js
@@ -0,0 +1,15 @@
+
+
+
+function meterToFoot(){
+    var meters = document.getElementById("meters").value;
+    var result =  3.279 * meters;
+    document.getElementById("result1").innerHTML = result + "feet";
+}
+
+function footToMeter(){
+    var feet = document.getElementById("feet").value;
+    var result = 0.305 * feet;
+    document.getElementById("result2").innerHTML = result + "meters";
+}
+
