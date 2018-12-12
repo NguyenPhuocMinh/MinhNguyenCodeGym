e67b85d4b8eb3e65f4f8cf454d8d78f840eed32a
NguyenPhuocMinh <Xiuquay55@gmail.com>
2018-12-12 17:03:05 +0700

    BT_Luyện Tập Tạo Và Sử Dụng Hàm

--- 06.Function/BT_ChuyenDoiFeetVaMeter/index.js
+++ 06.Function/BT_ChuyenDoiFeetVaMeter/index.js
@@ -4,12 +4,12 @@
 function meterToFoot(){
     var meters = document.getElementById("meters").value;
     var result =  3.279 * meters;
-    document.getElementById("result1").innerHTML = result + "feet";
+    document.getElementById("result1").innerHTML = result + " feet";
 }
 
 function footToMeter(){
     var feet = document.getElementById("feet").value;
     var result = 0.305 * feet;
-    document.getElementById("result2").innerHTML = result + "meters";
+    document.getElementById("result2").innerHTML = result + " meters";
 }
 
